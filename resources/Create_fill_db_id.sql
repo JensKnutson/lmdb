a29529e15c6690aa5a512fbfa683ea1cb02c708b
Johan Cederschiold <johan.cederschiold@gmail.com>
2019-04-30 15:04:00 +0200

    Fill script fixed

---
+++ resources/Create_fill_db_id.sql
@@ -0,0 +1,23 @@
+create table if not exists movie
+(id integer, Title text, year integer, format text);
+insert into movie
+(id, title, year, format)
+values 
+(1, 'Johan Falk - Gruppen för särskilda insatser ', 2009, null),
+(2, 'Johan Falk: Kodnamn: Lisa ', 2012, null),
+(3, 'Johan Falk: National Target ', 2009, 'Video'),
+(4, 'Johan Falk: Spelets regler ', 2012, 'Video'),
+(5, 'Johan Falk: Blodsdiamanter ', 2015, 'Video'),
+(6, 'Johan Falk: Slutet ', 2015, 'Video'),
+(7, 'Johan Falk: Vapenbröder ', 2009, 'Video'),
+(8, 'Johan Falk: Barninfiltratören ', 2012, 'Video'),
+(9, 'Johan Falk: Organizatsija Karayan ', 2012, 'Video'),
+(10, 'Johan Falk: Leo Gaut ', 2009, 'Video'),
+(11, 'Johan Falk: Lockdown ', 2015, 'Video'),
+(12, 'Johan Falk: De fredlösa ', 2009, 'Video'),
+(13, 'Johan Falk: Tyst diplomati ', 2015, 'Video'),
+(14, 'Johan Falk: Operation Näktergal ', 2009, 'Video'),
+(15, 'Johan Falk: De 107 patrioterna ', 2012, 'Video'),
+(16, 'Johan Falk: Ur askan i elden ', 2015, 'Video'),
+(17, 'Johan Falk: Alla råns moder ', 2012, 'Video');
+
