941648bde4428e22d5dc0f220797e6803a5da019
Jens <Jens.knutson@outlook.com>
2019-04-30 14:28:52 +0200

    Added produces json to removeMovie

--- src/main/java/com/lmdb/rest/MoviesResource.java
+++ src/main/java/com/lmdb/rest/MoviesResource.java
@@ -4,23 +4,17 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-
 import javax.ws.rs.Consumes;
-
 import javax.ws.rs.DELETE;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
 import javax.ws.rs.ServiceUnavailableException;
-import javax.ws.rs.core.Response;
-
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
-
+import javax.ws.rs.core.Response;
 
 import com.lmdb.domain.Movie;
 import com.lmdb.management.LmdbServiceLocal;
@@ -59,6 +53,7 @@ public class MoviesResource {
 	
 
 	@DELETE
+	@Produces("application/JSON")
 	@Path("/delete/{id}")
 	public Movie removeMovie(@PathParam("id") int id, @Context HttpHeaders header) {
 		return lmdb.deleteMovie(id);
