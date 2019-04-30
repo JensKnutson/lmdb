a9813eaca44ca86630435d55df60c0759fe3cf16
Johan Cederschiold <johan.cederschiold@gmail.com>
2019-04-30 13:31:30 +0200

    Fixed implementation

--- src/main/java/com/lmdb/management/LmdbServiceImplementation.java
+++ src/main/java/com/lmdb/management/LmdbServiceImplementation.java
@@ -2,13 +2,21 @@ package com.lmdb.management;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import com.lmdb.dataaccess.LmdbDataAccess;
 import com.lmdb.domain.Movie;
 
+@Stateless
 public class LmdbServiceImplementation implements LmdbServiceLocal {
+	
+	@Inject
+	LmdbDataAccess lda;
 
 	@Override
 	public List<Movie> getAllMovies() {
-		return null;
+		return lda.getAllMovies();
 	}
 
 }
